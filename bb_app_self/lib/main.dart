895cde2d5973417794ca6da6913eee34f6232cd8
Gautam <gk9765551@gmail.com>
2023-02-06 19:15:51 +0530

    profile page done

--- bb_app_self/lib/main.dart
+++ bb_app_self/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:bb_app_self/screens/screen1.dart';
+import 'package:bb_app_self/screens/community.dart';
 
 void main() {
   runApp(const MyApp());
